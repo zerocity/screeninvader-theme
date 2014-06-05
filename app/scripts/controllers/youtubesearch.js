1521a2d1791070031ebac0ea5b187691d8bbf185
Philpp Bisson <philipp.bisson@gmail.com>
2014-06-05 04:07:45 +0200

    add angular upload

--- app/scripts/controllers/youtubesearch.js
+++ app/scripts/controllers/youtubesearch.js
@@ -47,4 +47,26 @@ angular.module('09ScreeninvaderApp')
   $scope.postUrl = function() {
     JanoshDriver.addItem($scope.postUrlQuery)
   }
+
+  $scope.acceptTypes = 'video/*,audio/*,image/*';
+
+  $scope.onUpload = function(file) {
+    console.log('upload',file);
+    $scope.message = file[0].name + ' will be uploaded';
+  }
+
+  $scope.onError = function(res) {
+    console.log('error',res);
+  }
+
+  $scope.onSuccess = function(res) {
+    console.log('success',res);
+  }
+
+  $scope.onComplete = function(res) {
+    console.log('Complete',res);
+    $scope.message = 'Upload success';
+  }
+
+
 });
