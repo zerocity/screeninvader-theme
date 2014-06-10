ada5f1a92be46feb957249c91acfb0947ca538a2
Philpp Bisson <philipp.bisson@gmail.com>
2014-06-10 21:29:48 +0200

    rewrite youtube search with keystrok delay and change the ui

--- app/scripts/controllers/youtubesearch.js
+++ app/scripts/controllers/youtubesearch.js
@@ -1,47 +1,19 @@
 'use strict';
 
 angular.module('09ScreeninvaderApp')
-  .controller('YoutubesearchCtrl', function ($scope,$http,$timeout,JanoshDriver) {
+  .controller('YoutubesearchCtrl', function ($scope,$http,$timeout,JanoshDriver,Youtubeapi) {
+
+  $scope.YoutubeResults = {}
 
   $scope.getUrl = function(key) {
     $('#'+key).toggleClass('hide').focus();
   };
 
-  $scope.youtubeAPI = function() {
-    var max_videos = 6;
-    var parameters = {
-      params: {
-          type: 'video',
-          'max-results':max_videos,
-          alt:'json',
-          q: $scope.query
-        }
-      };
-    var youtubeApiV3Url = 'https://www.googleapis.com/youtube/v3/search'; // require key parameter
-    var youtubeApiV1Url = "http://gdata.youtube.com/feeds/api/videos";
-
-    $http.get(youtubeApiV1Url,parameters)
-      .success( function (result) {
-        var data = _.map(result.feed.entry, function (num,key){
-            var a = num.id.$t.split("/"),
-                id = a[6],
-                title = num.title.$t,
-                thumbnail = num.media$group.media$thumbnail[1].url,
-                description = num.media$group.media$description.$t;
-            return {
-              source:"http://www.youtube.com/watch?v="+id,
-              youtubeId:id,
-              title:title,
-              thumbnail:thumbnail,
-              description:description
-            };
-        });
-        $scope.YoutubeResults = data;
-        //console.log($scope.YoutubeResults);
-      })
-      .error(function(result) {
-        console.log(result);
-      });
+  $scope.searchYoutube = function(term) {
+    return Youtubeapi.searchYoutube(term).then(function (result){
+      $scope.YoutubeResults = result
+      return result
+    });
   };
 
   $scope.postUrl = function() {
@@ -67,6 +39,5 @@ angular.module('09ScreeninvaderApp')
     console.log('Complete',res);
     $scope.message = 'Upload success';
   }
-
 
 });
